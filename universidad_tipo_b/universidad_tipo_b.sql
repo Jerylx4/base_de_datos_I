9cdbd6908b772a2ee52fce3c7b6b98d91fa8750a
Jerylx4 <145602365+Jerylx4@users.noreply.github.com>
2025-05-12 15:50:04 -0500

    Update universidad_tipo_b.sql

--- universidad_tipo_b/universidad_tipo_b.sql
+++ universidad_tipo_b/universidad_tipo_b.sql
@@ -1,3 +1,82 @@
+--
+-- PostgreSQL database dump
+--
+-- Dumped from database version 17.4
+-- Dumped by pg_dump version 17.4
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: universidad; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA universidad;
+
+ALTER SCHEMA universidad OWNER TO postgres;
+
+--
+-- Name: sexo_enum; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.sexo_enum AS ENUM (
+    'H',
+    'M'
+);
+
+ALTER TYPE public.sexo_enum OWNER TO postgres;
+
+--
+-- Name: tipo_asignatura_enum; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.tipo_asignatura_enum AS ENUM (
+    'básica',
+    'obligatoria',
+    'optativa'
+);
+
+ALTER TYPE public.tipo_asignatura_enum OWNER TO postgres;
+
+--
+-- Name: sexo_enum; Type: TYPE; Schema: universidad; Owner: postgres
+--
+
+CREATE TYPE universidad.sexo_enum AS ENUM (
+    'H',
+    'M'
+);
+
+ALTER TYPE universidad.sexo_enum OWNER TO postgres;
+
+--
+-- Name: tipo_asignatura_enum; Type: TYPE; Schema: universidad; Owner: postgres
+--
+
+CREATE TYPE universidad.tipo_asignatura_enum AS ENUM (
+    'básica',
+    'obligatoria',
+    'optativa'
+);
+
+ALTER TYPE universidad.tipo_asignatura_enum OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: alumno_se_matricula_asignatura; Type: TABLE; Schema: public; Owner: postgres
+--
+
 CREATE TABLE universidad.departamento (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
