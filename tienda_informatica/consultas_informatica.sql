0a759e2a40e3a3a74c3b44ce63d51c2130909d6b
Ronny199717 <ronaldcacuango17@gmail.com>
2025-05-22 14:34:00 -0500

    Update consultas_informatica.sql

--- tienda_informatica/consultas_informatica.sql
+++ tienda_informatica/consultas_informatica.sql
@@ -1,5 +1,70 @@
 --1.1.3 Consultas sobre una tabla
 
+--18.Lista el nombre y el precio del producto más barato. (Utilice solamente las cláusulas ORDER BY y LIMIT).
+SELECT t.nombre,t.precio
+FROM tienda_informatica.producto t
+ORDER BY t.precio asc 
+LIMIT 1;
+--19.Lista el nombre y el precio del producto más caro. (Utilice solamente las cláusulas ORDER BY y LIMIT).
+SELECT t.nombre,t.precio
+FROM tienda_informatica.producto t
+ORDER BY t.precio desc 
+LIMIT 1;
+--20.Lista el nombre de todos los productos del fabricante cuyo identificador de fabricante es igual a 2.
+SELECT nombre
+FROM tienda_informatica.producto
+WHERE id_fabricante = 2;
+
+--21.Lista el nombre de los productos que tienen un precio menor o igual a 120€.
+SELECT nombre,precio as precio_USD,(precio*0.93) as precio_Euro
+FROM tienda_informatica.producto
+WHERE (precio*0.93) <= 120;
+
+--22.Lista el nombre de los productos que tienen un precio mayor o igual a 400€.
+SELECT nombre,precio as precio_USD,(precio*0.93) as precio_Euro
+FROM tienda_informatica.producto
+WHERE (precio*0.93)>= 400;
+
+
+--23.Lista el nombre de los productos que no tienen un precio mayor o igual a 400€.
+SELECT nombre,precio as precio_USD,(precio*0.93) as precio_Euro
+FROM tienda_informatica.producto
+WHERE NOT (precio*0.93)>= 400;
+
+--24.Lista todos los productos que tengan un precio entre 80€ y 300€. Sin utilizar el operador BETWEEN.
+SELECT nombre,precio as precio_USD,(precio*0.93) as precio_Euro
+FROM tienda_informatica.producto
+WHERE (precio*0.93)>= 80 AND (precio*0.93)<=300;
+
+--25.Lista todos los productos que tengan un precio entre 60€ y 200€. Utilizando el operador BETWEEN.
+SELECT nombre,precio as precio_USD,(precio*0.93) as precio_Euro
+FROM tienda_informatica.producto
+WHERE (precio*0.93) BETWEEN 60 AND 200;
+
+--26.Lista todos los productos que tengan un precio mayor que 200€ y que el identificador de fabricante sea igual a 6.
+SELECT *,(precio*0.93) as precio_Euro
+FROM tienda_informatica.producto
+WHERE (precio*0.93)>=200 AND id_fabricante=6;
+
+--27.Lista todos los productos donde el identificador de fabricante sea 1, 3 o 5. Sin utilizar el operador IN.
+SELECT *
+FROM tienda_informatica.producto
+WHERE id_fabricante=1 OR id_fabricante=3 OR id_fabricante=5;
+
+--28.Lista todos los productos donde el identificador de fabricante sea 1, 3 o 5. Utilizando el operador IN.
+SELECT *
+FROM tienda_informatica.producto
+WHERE id_fabricante IN (1,3,5);
+
+--29.Lista el nombre y el precio de los productos en céntimos (Habrá que multiplicar por 100 el valor del precio). Cree un alias para la columna que contiene el precio que se llame céntimos.
+SELECT p.nombre,p.precio*100 AS centimos
+FROM tienda_informatica.producto p;
+
+--30.Lista los nombres de los fabricantes cuyo nombre empiece por la letra S.
+
+SELECT f.nombre
+FROM tienda_informatica.fabricante f
+WHERE f.nombre LIKE 'S%';
 --1.1.4 Consultas multitabla (Composición interna)
 
 --1.1.5 Consultas multitabla (Composición externa)
