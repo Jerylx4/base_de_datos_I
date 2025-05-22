5edc9bf7247ef93d5dc6800d1a7817d99b11552b
Jerylx4 <145602365+Jerylx4@users.noreply.github.com>
2025-05-22 14:36:31 -0500

    Update consultas_gestion_ventas.sql

--- genstion_ventas/consultas_gestion_ventas.sql
+++ genstion_ventas/consultas_gestion_ventas.sql
@@ -47,7 +47,33 @@
   WHERE cliente.nombre = 'María' AND cliente.apellido1 = 'Santana' AND cliente.apellido2 = 'Moreno'
   ORDER BY comercial.apellido1 ASC, comercial.apellido2 ASC;
 
+  --7. Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial Daniel Sáez Vega.
+  SELECT * FROM gestion_ventas.cliente
+  SELECT * FROM gestion_ventas.pedido
+  SELECT * FROM gestion_ventas.comercial
+  SELECT DISTINCT c.nombre,c.apellido1,c.apellido2 FROM gestion_ventas.cliente c
+  INNER JOIN gestion_ventas.pedido p ON p.id_cliente=c.id
+  INNER JOIN gestion_ventas.comercial co ON co.id=p.id_comercial
+  WHERE co.id=1
+
 -- 1.3.5 Consultas multitabla (Composición externa)
+  --1. Devuelve un listado con todos los clientes junto con los datos de los pedidos que han realizado. Este listado también debe incluir los clientes que no han realizado ningún pedido.
+  --El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los clientes.
+  SELECT  c.apellido1,c.apellido2,c.nombre,p.id,p.total,p.fecha,p.id_cliente,p.id_comercial FROM gestion_ventas.cliente c
+  FULL OUTER JOIN gestion_ventas.pedido p ON p.id_cliente=c.id
+  --GROUP BY c.apellido1,c.apellido2,c.nombre,p.id,p.total,p.fecha,p.id_cliente,p.id_comercial
+  ORDER BY c.apellido1 ASC
+  
+  --2. Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado. Este listado también debe incluir los comerciales que no han realizado ningún pedido. 
+  --El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los comerciales.
+  SELECT co.apellido1,co.apellido2,co.nombre,co.comision,p.total,p.id_cliente,p.id_comercial FROM gestion_ventas.comercial co
+  FULL OUTER JOIN gestion_ventas.pedido p ON co.id=p.id_comercial
+  ORDER BY co.apellido1 ASC
+      
+  --3. Devuelve un listado que solamente muestre los clientes que no han realizado ningún pedido.
+  SELECT * FROM gestion_ventas.cliente c
+  LEFT JOIN gestion_ventas.pedido p ON c.id=p.id_cliente
+  WHERE p.id IS NULL
 
 -- 1.3.6 Consultas resumen
 
