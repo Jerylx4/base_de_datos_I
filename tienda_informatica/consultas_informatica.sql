c6dac5564cc6c216c845b4b2ac613b55e4552bba
Ronny199717 <ronaldcacuango17@gmail.com>
2025-05-22 14:37:30 -0500

    Update consultas_informatica.sql

--- tienda_informatica/consultas_informatica.sql
+++ tienda_informatica/consultas_informatica.sql
@@ -1,4 +1,44 @@
 --1.1.3 Consultas sobre una tabla
+--1.Lista el nombre de todos los productos que hay en la tabla producto.
+SELECT nombre  FROM tienda_informatica.producto;
+
+--2.Lista los nombres y los precios de todos los productos de la tabla producto.
+SELECT nombre, precio FROM tienda_informatica.producto;
+
+--3.Lista todas las columnas de la tabla producto.
+SELECT * FROM tienda_informatica.producto;
+
+--4.Lista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD).
+SELECT nombre, precio AS euros, precio * 1.07 AS dolares 
+FROM tienda_informatica.producto;
+
+--5.Lista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD). 
+--Utiliza los siguientes alias para las columnas: nombre de producto, euros, dólares.
+SELECT nombre AS "nombre de producto", precio AS euros, precio * 1.07 AS dolares 
+FROM tienda_informatica.producto;
+
+--6.Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a mayúscula.
+SELECT UPPER(nombre), precio 
+FROM tienda_informatica.producto;
+
+--7.Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a minúscula.
+SELECT LOWER(nombre), precio 
+FROM tienda_informatica.producto;
+
+--8.Lista el nombre de todos los fabricantes en una columna, y en otra columna obtenga en mayúsculas los 
+--dos primeros caracteres del nombre del fabricante.
+SELECT nombre, UPPER(SUBSTRING(nombre, 1, 2)) AS primeros_caracteres 
+FROM tienda_informatica.fabricante;
+
+--9.Lista los nombres y los precios de todos los productos de la tabla producto, redondeando el valor del precio.
+SELECT nombre, ROUND(precio) AS precio_redondeado 
+FROM tienda_informatica.producto;
+
+--10.Lista los nombres y los precios de todos los productos de la tabla producto, 
+--truncando el valor del precio para mostrarlo sin ninguna cifra decimal.
+SELECT nombre, TRUNC(precio) AS precio_truncado 
+FROM tienda_informatica.producto;
+
 
 --18.Lista el nombre y el precio del producto más barato. (Utilice solamente las cláusulas ORDER BY y LIMIT).
 SELECT t.nombre,t.precio
