40e66d6d604cc0058c94ba4c31eb7edccd0dffff
AnthonyJ0416 <anthonyjoseph0416@gmail.com>
2025-05-22 15:14:21 -0500

    Update consultas_gestion_empleados.sql

--- gestion_empleados/consultas_gestion_empleados.sql
+++ gestion_empleados/consultas_gestion_empleados.sql
@@ -76,6 +76,64 @@ FROM gestion_empleados.empleado;
   SELECT nombre,gastos FROM departamento
   ORDER BY gastos ASC
   LIMIT 2
+      
+  --20. Devuelve una lista con 5 filas a partir de la tercera fila de la tabla empleado. La tercera fila se debe incluir en la respuesta. La respuesta debe incluir todas las columnas de la tabla empleado.
+  SELECT * FROM gestion_empleados.empleado LIMIT 5 OFFSET 2;
+  
+  --21. Devuelve una lista con el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto mayor o igual a 150000 euros.
+  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE presupuesto >=150000 ORDER BY presupuesto ASC
+  
+  --22. Devuelve una lista con el nombre de los departamentos y el gasto, de aquellos que tienen menos de 5000 euros de gastos.
+  SELECT nombre, gastos FROM gestion_empleados.departamento WHERE gastos <= 5000 ORDER BY gastos ASC
+  
+  --23. Devuelve una lista con el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.
+  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE (presupuesto>=100000 AND presupuesto <=200000) ORDER BY presupuesto ASC
+  
+  --24. Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.
+  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE (presupuesto>=200000 OR presupuesto<=100000)
+  
+  --25. Devuelve una lista con el nombre de los departamentos que tienen un presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
+  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE presupuesto BETWEEN 100000 AND 200000 ORDER BY presupuesto ASC
+  
+  --26. Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
+  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE presupuesto NOT BETWEEN 100000 AND 200000 ORDER BY presupuesto ASC
+
+--27.Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, de aquellos departamentos donde los gastos sean mayores que el presupuesto del que disponen./
+select departamento,gastos,presupuesto 
+from empresa.departamento
+where gastos > presupuesto;
+--28.Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, de aquellos departamentos donde los gastos sean menores que el presupuesto del que disponen./
+select departamento,gastos, presupuesto 
+from empresa.departamento
+where gastos < presupuesto;
+--29.Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, de aquellos departamentos donde los gastos sean iguales al presupuesto del que disponen./
+select departamento,gastos, presupuesto 
+from empresa.departamento
+where gastos = presupuesto;
+--30.Lista todos los datos de los empleados cuyo segundo apellido sea NULL./
+select * from empresa.empleado 
+where apellido2 is null;
+--31.Lista todos los datos de los empleados cuyo segundo apellido no sea NULL./
+select *from empresa.empleado
+where apellido2 is not null;
+--32. Lista todos los datos de los empleados cuyo segundo apellido sea López./
+select *from empresa.empleado
+where apellido2 ilike'lópez';
+--33.Lista todos los datos de los empleados cuyo segundo apellido sea Díaz o Moreno. Sin utilizar el operador IN./
+select*from empresa.empleado
+where apellido2 ilike 'díaz' or apellido2 ilike 'moreno';
+--34.Lista todos los datos de los empleados cuyo segundo apellido sea Díaz o Moreno. Sin utilizar el operador IN./
+select*from empresa.empleado
+where apellido2 in  ('Díaz','Moreno');
+--35.Lista los nombres, apellidos y nif de los empleados que trabajan en el departamento 3./
+select nombre,apellido1,apellido2, nif 
+from empresa.empleado 
+where id_departamento =3;
+--36.Lista los nombres, apellidos y nif de los empleados que trabajan en los departamentos 2, 4 o 5./
+select nombre,apellido1,apellido2,nif
+from empresa.empleado
+where id_departamento in (2,4,5);      
+
 
   -- 1.2.4 Consultas multitabla (Composición interna)
   --7. Devuelve un listado con los datos de los empleados que trabajan en el --departamento de I+D. Ordena el resultado alfabéticamente.
@@ -227,27 +285,6 @@ FROM gestion_empleados.empleado;
   --7. Calcula el número total de empleados que hay en la tabla empleado.
   SELECT DISTINCT COUNT(id)
   FROM "empleado".empleado;
-
-  --20. Devuelve una lista con 5 filas a partir de la tercera fila de la tabla empleado. La tercera fila se debe incluir en la respuesta. La respuesta debe incluir todas las columnas de la tabla empleado.
-  SELECT * FROM gestion_empleados.empleado LIMIT 5 OFFSET 2;
-  
-  --21. Devuelve una lista con el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto mayor o igual a 150000 euros.
-  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE presupuesto >=150000 ORDER BY presupuesto ASC
-  
-  --22. Devuelve una lista con el nombre de los departamentos y el gasto, de aquellos que tienen menos de 5000 euros de gastos.
-  SELECT nombre, gastos FROM gestion_empleados.departamento WHERE gastos <= 5000 ORDER BY gastos ASC
-  
-  --23. Devuelve una lista con el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.
-  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE (presupuesto>=100000 AND presupuesto <=200000) ORDER BY presupuesto ASC
-  
-  --24. Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.
-  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE (presupuesto>=200000 OR presupuesto<=100000)
-  
-  --25. Devuelve una lista con el nombre de los departamentos que tienen un presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
-  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE presupuesto BETWEEN 100000 AND 200000 ORDER BY presupuesto ASC
-  
-  --26. Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
-  SELECT nombre, presupuesto FROM gestion_empleados.departamento WHERE presupuesto NOT BETWEEN 100000 AND 200000 ORDER BY presupuesto ASC
 
 -- 1.2.7 Subconsultas
 
